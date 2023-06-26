914d11668d8cac0efcf7b693a1c8e3bdced77121
NewStrongBox <nathan.lee.thompson@gmail.com>
2023-06-26 11:34:16 -0500

    code cleanup

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError
+from dotenv import load_dotenv
 import random
 import requests
 import openai 
@@ -12,19 +13,26 @@ import os
 
 
 """
-Initialize the application and the database
+Initialize the application, database, and variables
 """
 
+load_dotenv()
+
 app = Flask(__name__)
-# app.secret_key = "TOTALLYSECRETKEY" # uh, let's change this soon - or remove, not using Sessions
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proj361.db'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 db = SQLAlchemy(app)
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 the_time = datetime.now()
 the_date = the_time.strftime('%m/%d/%Y')
+langs = {"English": "en",
+         "Spanish": "es",
+         "Hindi": "hi",
+         "Chinese": "zh",
+         "French": "fr"}
+
 
 class Pitch(db.Model):
     """
@@ -40,11 +48,9 @@ class Pitch(db.Model):
     # change to default not nullable
     deleted = db.Column(db.Boolean, nullable=False)
 
-    # with app.app_context():
-    #     db.create_all()
-
     def __repr__(self):
         return '<Pitches %r>' % self.id
+
 
 """
 Routes for different functions
@@ -57,7 +63,7 @@ def home():
 def make():
     if request.method == 'POST':
         # 1. get the original word from the user:
-        prompt, language = request.form['prompt'], request.form['language']
+        prompt, language = request.form['prompt'], langs[request.form['language']]
         if request.form['prompt'] == '':
             return redirect('/action_fail')
         
@@ -65,11 +71,11 @@ def make():
         words = get_words(prompt, language)
 
         # 3. get summary from Microservice
-        summary = get_summary(words[0], words[1], words[2])
-        print(summary)
+        # summary = get_summary(words[0], words[1], words[2])
+        # print(summary)
 
         # 4. get AI prompt paragraph
-        ai_prompt = generate_prompt(words, summary, language)
+        ai_prompt = generate_prompt(words, language)
 
         # 5. generate pitch from prompt
         new_pitch = pitch(ai_prompt)
@@ -95,7 +101,6 @@ def nope():
 def view(id):
     idea = Pitch.query.get_or_404(id)
     return render_template('display.html', idea=idea)
-    # return render_template(url_for('view'), id=id, idea=idea)
 
 @app.route('/show_all', methods=['GET'])
 def show_em():
@@ -107,13 +112,12 @@ def edit_idea(id):
     idea = Pitch.query.get_or_404(id)
     if request.method == 'GET':
         return render_template('/edit.html', idea=idea)
-    # if request.method == 'POST':
     idea.pitch = request.form['pitch']
     try:
         db.session.commit()
         return redirect('/action')
     except:
-        return "dunno, didn\'t edit tho"
+        return({"Error: Edit failed"}, 400)
     
 # this just changes the value and displays on deleted page
 @app.route('/delete/<id>', methods=['GET'])
@@ -127,7 +131,7 @@ def first_del(id):
         db.session.commit()
         return redirect('/deleted')
     except:
-        return "dunno, didn\'t delete tho"
+        return({"Error: Delete failed"}, 400)
     
 # this permanently deletes 
 @app.route('/deleted/<id>', methods=['GET'])
@@ -138,7 +142,7 @@ def delete_idea(id):
         db.session.commit()
         return redirect('/action')
     except:
-        return "dunno, didn\'t delete tho"
+        return({"Error: Delete failed"}, 400)
 
 @app.route('/deleted', methods=['GET'])
 def show_deleted():
@@ -162,7 +166,6 @@ def get_words(prompt, language):
     while res is None:
         res = get_res(prompt, language)
     words = get_links(res)
-    # print(finals)
     return words
 
 def get_res(prompt, language):
@@ -170,20 +173,11 @@ def get_res(prompt, language):
     Returns None if a Wiki page is not found or contents if valid URL
     """
     try:
-        # prompt = input("Enter a word or phrase below - 2 words max! Prompt:  ")
+        print(f"THE LANGUAGE IS {language}")
         if prompt == ' ':
             return None
-        if language == 'Spanish':
-            str_url = 'https://es.wikipedia.org/w/index.php?search=%s' % prompt
-        elif language == 'Hindi':
-            str_url = 'https://hi.wikipedia.org/w/index.php?search=%s' % prompt
-        elif language == 'French':
-            str_url = 'https://fr.wikipedia.org/w/index.php?search=%s' % prompt
-        elif language == 'Chinese':
-            str_url = 'https://zh.wikipedia.org/w/index.php?search=%s' % prompt
-        else:
-            str_url = 'https://en.wikipedia.org/w/index.php?search=%s' % prompt
-        # print(str_url)
+        base_url = 'https://%s.wikipedia.org/w/index.php?search=' % language
+        str_url = base_url + prompt
         response = requests.get(str_url)
         response.raise_for_status()
     except HTTPError:
@@ -198,8 +192,6 @@ def get_links(res):
     """
     result1 = BeautifulSoup(res.content, 'html.parser')
     title = result1.find('title').get_text()
-    print(title)
-    # check to see if this works...nope, doesn't yet! 
     if title[-26:-1] == 'Search results - Wikipedia':
         print("Looks like your search did not yield any results - try again!")
         get_words()
@@ -227,9 +219,18 @@ def check_valid(term):
     Ideally you'd use a regex here to weed out the many edge cases
 
     """
-    not_valid = ['', ' ', '.', '..', '^', 'a', 'b', 't', 'All article disambiguation pages', 'Short description matches Wikidata', 
-                 'Pages including recorded pronunciations', 'See also', 'All disambiguation pages', 'learn more', 'All articles that may contain original research', 'edit',
-                 'Related changes', 'Contributions', 'Archived', 'Talk', 'Log in', 'Mobile view', 'What links here', 'Page information', 'Privacy Policy', 'Disambiguation', 'Special Pages']
+    not_valid = [
+                 '', ' ', '.', '..', '^', 'a', 'b', 't', 
+                 'All article disambiguation pages', 
+                 'Short description matches Wikidata', 
+                 'Pages including recorded pronunciations', 'See also', 
+                 'All disambiguation pages', 'learn more', 
+                 'All articles that may contain original research',
+                 'edit', 'Related changes', 'Contributions', 'Archived', 
+                 'Talk', 'Log in', 'Mobile view', 'What links here', 
+                 'Page information', 'Privacy Policy', 'Disambiguation', 
+                 'Special Pages'
+                 ]
 
     if term in not_valid or 'ikipedia' in term:
         return False
@@ -246,6 +247,7 @@ def pitch(ai_prompt):
     This will call the OpenAI AP to with a prompt to create a pitch using the
     three words that were generated from the prompt through the get_words() function
     """
+    print(f"\nTHE OPENAI KEY IS {openai.api_key}\n")
     pitch = openai.Completion.create(
             model="text-davinci-003",
             prompt=ai_prompt,
@@ -257,42 +259,18 @@ def pitch(ai_prompt):
     return pitch.choices[0].text
 
 
-def generate_prompt(words, summary, language):
+def generate_prompt(words, language):
     """
     Returns the prompt that will be sent to the OpenAI endpoint.
     """
-    print(f"THE LANGUAGE SHOULD BE {language}\n")
     return f"""Write a killer pitch, in {language} for VC investors for a 
     startup involving three random terms. DO NOT include the terms 'investor',
       'VC', 'venture capital', or 'startup'. It should be extremely 
     compelling and informed, erudite and witty, with advanced 
     vocabulary terms and outrageous claims about the efficacy of the product.
-      Incorporate the feeling from this wikipedia page: {summary}"
      The terms are {words[0].capitalize()}, {words[1].capitalize()}, and 
      {words[2].capitalize()} The pitch should be in {language}."""
-
-
-# this will use the microservice
-def get_summary(one, two, three):
-    api_url = 'https://fejxhfgkg7.execute-api.us-east-2.amazonaws.com/Beta/search'
-    headers = {'x-api-key': os.environ.get('SECOND_KEY')}
-    try:
-        summary = requests.get(api_url, params={'one': one, 'two': two, 'three': three}, headers=headers)
-        # summary.raise_for_status()
-    except HTTPError:
-        print("That query didn\'t work :( Try again!")
-        return None
-    else:
-        try: 
-            print(summary.json()['summary'])
-            return summary.json()['summary'] # add func to remove the '\n' etc
-        except KeyError:
-            pass
-        return(f"There's not much to say about {one}, {two}, {three} but try anyway.")
+\
 
 if __name__=='__main__':
-    # with app.app_context():
-    #     db.create_all()
-    print("CREATING THAT DB")
-    db.create_all()
     app.run(debug=True, port=8183)
